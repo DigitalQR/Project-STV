59f2831bb2b76e04b6b85f20853fc74094b5e754
Sam Potter <sam_pot_97@hotmail.com>
2016-04-14 22:41:58 +0100

    Fixed player motion

--- STV-Project/STV-Project/PlayerController.h
+++ STV-Project/STV-Project/PlayerController.h
@@ -2,6 +2,7 @@
 #include "Component.h"
 #include "Camera.h"
 #include "Keyboard.h"
+#define PI 3.141592
 
 class PlayerController : public Component
 {
@@ -14,41 +15,54 @@ public:
 
 	void LogicUpdate() 
 	{
+		Camera::getMain()->location = parent->location;
+		Camera::getMain()->rotation = parent->rotation;
+
 		vec3 direction = Camera::getMain()->getForward();
 		direction.y = 0;
 		float speed = 0.4f;
 		float sensitivity = 0.1f;
 
 		if (Keyboard::isKeyDown('a'))
-			Camera::getMain()->location -= cross(direction, vec3(0, 1, 0))*speed;
+			parent->location -= cross(direction, vec3(0, 1, 0))*speed;
 
 		if (Keyboard::isKeyDown('d'))
-			Camera::getMain()->location += cross(direction, vec3(0, 1, 0))*speed;
+			parent->location += cross(direction, vec3(0, 1, 0))*speed;
 
 		if (Keyboard::isKeyDown('w'))
-			Camera::getMain()->location += direction*speed;
+			parent->location += direction*speed;
 
 		if (Keyboard::isKeyDown('s'))
-			Camera::getMain()->location -= direction*speed;
+			parent->location -= direction*speed;
 
 		if (Keyboard::isKeyDown('e'))
-			Camera::getMain()->location += vec3(0, 1, 0)*speed;
+			parent->location += vec3(0, 1, 0)*speed;
 
 		if (Keyboard::isKeyDown('q'))
-			Camera::getMain()->location -= vec3(0, 1, 0)*speed;
+			parent->location -= vec3(0, 1, 0)*speed;
 
 
 		if (Keyboard::isKeyDown('j'))
-			Camera::getMain()->rotation += vec3(0.0f, sensitivity, 0.0f);
+			parent->rotation += vec3(0.0f, sensitivity, 0.0f);
 
 		if (Keyboard::isKeyDown('l'))
-			Camera::getMain()->rotation -= vec3(0.0f, sensitivity, 0.0f);
+			parent->rotation -= vec3(0.0f, sensitivity, 0.0f);
 
 		if (Keyboard::isKeyDown('i'))
-			Camera::getMain()->rotation += vec3(sensitivity, 0.0f, 0.0f);
+		{
+			parent->rotation += vec3(sensitivity, 0.0f, 0.0f);
+			if (parent->rotation.x >= PI / 2.0f)
+				parent->rotation.x = PI / 2.0f;
+		}
 
 		if (Keyboard::isKeyDown('k'))
-			Camera::getMain()->rotation -= vec3(sensitivity, 0.0f, 0.0f);
+		{
+			parent->rotation -= vec3(sensitivity, 0.0f, 0.0f);
+			if (parent->rotation.x <= -PI / 2.0f)
+				parent->rotation.x = -PI / 2.0f;
+		}
+
+		cout << parent->rotation.x << endl;
 	};
 
 	void VisualUpdate() {};
