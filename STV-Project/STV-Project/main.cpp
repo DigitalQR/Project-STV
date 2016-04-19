cd87b6164d99b86e36944297d53c14453d3a653a
DigitalQR (Laptop) <sam_pot_97@hotmail.com>
2016-04-19 22:52:33 +0100

    Added house at spawn

--- STV-Project/STV-Project/main.cpp
+++ STV-Project/STV-Project/main.cpp
@@ -25,7 +25,6 @@ void Start(bool smooth_state, int seed)
 
 
 	Entity* player = new Entity();
-	player->location = vec3(0, 0, 0);
 	player->AddComponent(new EllipsoidBody(vec3(1,1,1)));
 	player->AddComponent(new PlayerController());
 	player->AddComponent(new ResourcePlacer());
@@ -34,6 +33,8 @@ void Start(bool smooth_state, int seed)
 	Terrain* terrain = new Terrain(seed);
 	Entity* day_cycle_controller = new Entity();
 	day_cycle_controller->AddComponent(new DayCycleController());
+	const float height = terrain->GetChunkLoader()->GetChunk(0, 0, 0)->GetHeight(8, 8);
+	player->location = vec3(8, height + 1.5f, 8);
 
 	Scene* scene = new Scene();
 	scene->AddToScene(player);
