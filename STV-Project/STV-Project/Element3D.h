437b871fa7ff9793aabf6dba405af57fb2276163
DigitalQR (Laptop) <sam_pot_97@hotmail.com>
2016-04-19 13:16:43 +0100

    Added the ability to set preferred shaders

--- STV-Project/STV-Project/Element3D.h
+++ STV-Project/STV-Project/Element3D.h
@@ -4,9 +4,14 @@
 #include "Transform.h"
 #include "TexturedModel.h"
 
+
+class Shader;
+
+
 class Element3D : public Transform
 {
 public:
+	Shader* prefered_shader = nullptr;
 	bool enabled;
 	Element3D(TexturedModel* textured_model) : textured_model(textured_model) 
 	{
--- STV-Project/STV-Project/Entity.h
+++ STV-Project/STV-Project/Entity.h
@@ -64,11 +64,14 @@ protected:
 	
 	@params Desired shader
 	*/	
-	void AddElementsForRender(Shader* shader)
+	void AddElementsForRender(Shader* default_shader)
 	{
 		for (Element3D* element : _elements)
 		{
-			AddElementForRender(element, shader);
+			if(element->prefered_shader == nullptr)
+				AddElementForRender(element, default_shader);
+			else
+				AddElementForRender(element, element->prefered_shader);
 		}
 	}
 
